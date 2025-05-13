7f94f3f8ef8c52ef2d422524cf87d963583a2db6
Brayan Pilla <bpilla9393@uta.edu.ec>
2025-05-13 15:13:59 -0500

    Se añadio la clase ClasePilla

---
+++ src/repositorioremoto/ClasePilla.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositorioremoto;
+
+/**
+ *
+ * @author braya
+ */
+public class ClasePilla {
+
+    public ClasePilla() {
+        System.out.println("Mi nombre es Brayan Pilla");
+    }
+    
+}
